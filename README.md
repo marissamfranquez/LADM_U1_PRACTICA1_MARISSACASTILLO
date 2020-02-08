c2fb310c8cdc2a17d934204bff35c5dd4e780b44
marissamfranquez <mamicastillofr@ittepic.edu.mx>
2020-02-07 18:06:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LaborarioDeDispMoviles
---
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1/Main4Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.ladm_u1_practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
